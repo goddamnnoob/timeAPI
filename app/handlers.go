3d5a9b94658aeba075b13d96c9583e973a9053cd
Goddamnnoob <gowthamreddy547@gmail.com>
2021-09-12 11:14:57 +0530

    Implemented Handling multiple inputs

--- app/handlers.go
+++ app/handlers.go
@@ -1,28 +1,40 @@
 package app
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+type timeLoc struct {
+	Loc  string
+	Time string
+}
+
 func GetTime(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	queryValues := r.URL.Query()
+	rw.Header().Add("Content-Type", "application/json")
+	res := []timeLoc{}
 	if !queryValues.Has("tz") {
-		fmt.Fprintln(rw, time.Now())
+		res = append(res, timeLoc{"LOCAL TIME", time.Now().String()})
 	} else {
 		tz := queryValues.Get("tz")
 		if tz == "" {
-			fmt.Fprintln(rw, time.Now())
+			res = append(res, timeLoc{"LOCAL TIME", time.Now().String()})
 		} else {
-			loc, err := time.LoadLocation(tz)
-			if err != nil {
-				fmt.Fprintln(rw, "Invalid Input 404 error")
-			} else {
-				fmt.Fprintln(rw, tz+": "+time.Now().In(loc).String())
+			tzs := strings.Split(tz, ",")
+			for i := range tzs {
+				loc, err := time.LoadLocation(tzs[i])
+				if err != nil {
+					res = append(res, timeLoc{"Invalid Input", "404 Error"})
+				} else {
+					res = append(res, timeLoc{tzs[i], time.Now().In(loc).String()})
+				}
 			}
 		}
 	}
+	json.NewEncoder(rw).Encode(res)
 }
